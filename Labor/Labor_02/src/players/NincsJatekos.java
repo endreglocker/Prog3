648e6e90f09a659d5d1fda5bf351cad2863330c8
endreglocker <76169515+endreglocker@users.noreply.github.com>
2022-11-19 17:41:58 +0100

    Delete Labor directory

--- Labor/Labor_02/src/players/NincsJatekos.java
+++
@@ -1,7 +0,0 @@
-package players;
-
-public class NincsJatekos extends RuntimeException {
-    public NincsJatekos(String s) {
-        super(s);
-    }
-}
--- Labor/Labor_04/src/NameComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class NameComparator implements Comparator<Beer>{
-    @Override
-    public int compare(Beer t, Beer t1) {
-            return t.getName().compareTo(t1.getName());
-    }
-}
